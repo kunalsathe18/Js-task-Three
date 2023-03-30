45e3c49a1d1eb567fe222e098da058d078e21966
kunalsathe18 <kunalsathe18@gmail.com>
2023-03-30 12:57:03 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var ViratKohli= 18;
+var king_kohli= 18;
+ViratKohli="Ms Dhoni said";
+king_kohli="Face the failure until failure fail to face you.";
+
+console.log(ViratKohli);
+console.log(king_kohli);
+// var varibale ends here
+
+let runmachine="Virat Kohli Said";
+let fan="Practice And Hardwork Always Earn You Success."
+console.log(runmachine);
+console.log(fan);
+// let varibale ends here
+
+const cricket= "Sachin Tendulkar Said";
+const bestbatsman= "People Throw Stones At You And You Convert Them into Milestones.";
+console.log(cricket);
+console.log(bestbatsman);
+// const varibale ends here
